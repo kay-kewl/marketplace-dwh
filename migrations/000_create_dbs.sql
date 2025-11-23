df34637c6d127960f58698b01104bc43bfb27e95
Kirill Komov <118173356+kay-kewl@users.noreply.github.com>
2025-11-23 19:26:26 +0000

    fix(db): make database creation idempotent

--- migrations/000_create_dbs.sql
+++ migrations/000_create_dbs.sql
@@ -1,3 +1,8 @@
-CREATE DATABASE user_service_db;
-CREATE DATABASE order_service_db;
-CREATE DATABASE logistics_service_db;
+SELECT 'CREATE DATABASE user_service_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'user_service_db')\gexec
+
+SELECT 'CREATE DATABASE order_service_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'order_service_db')\gexec
+
+SELECT 'CREATE DATABASE logistics_service_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'logistics_service_db')\gexec
