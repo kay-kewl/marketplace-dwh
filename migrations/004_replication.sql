0a81648bb149f6444ee2a698986560825fa1f8be
Kirill Komov <kidmkomov@edu.hse.ru>
2025-11-24 00:46:32 +0300

    fix(db): add semicolon

--- migrations/004_replication.sql
+++ migrations/004_replication.sql
@@ -3,6 +3,6 @@ DO $$
         IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'replicator') THEN
         CREATE ROLE replicator WITH REPLICATION LOGIN PASSWORD 'replicator';
         END IF;
-    END
+    END;
 $$;
 
