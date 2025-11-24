3e10b20731a27194c9e611e35c7a7b5e11f42f80
Kirill Komov <118173356+kay-kewl@users.noreply.github.com>
2025-11-24 08:29:18 +0000

    feat(ha): implement patroni cluster with etcd and haproxy

--- ha/entrypoint.sh
+++ ha/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
 
-exec patroni
+DATA_DIR="${PATRONI_POSTGRESQL_DATA_DIR:-/var/lib/postgresql/data/patroni}"
+
+mkdir -p "$DATA_DIR"
+chown -R postgres:postgres "$DATA_DIR"
+chmod 0700 "$DATA_DIR"
+
+exec gosu postgres "$@"
